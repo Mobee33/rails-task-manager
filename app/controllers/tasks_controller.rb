d4f126a2ce7387b811d45efe4adf633f9043cf28
Mubeen Bankur <mobee33@hotmail.com>
2022-05-07 13:44:41 +0400

    completed the showall and showonly one

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,34 @@
 class TasksController < ApplicationController
 
   def index
+    @tasks = Task.all
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Tasks.new(params[:task])
+    @task.save
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def edit
+    @task = task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
+    @task = Task.update(params[:task])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to restaurants_path
+  end
 end
